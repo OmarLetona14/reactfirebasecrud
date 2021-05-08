ca38d445b18468e913ef5603a9b7ac426ce40e6d
Omar Letona <omarletonaf@gmail.com>
2021-05-08 08:17:36 -0600

    Added show table method

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,6 +1,4 @@
 import React,{useState} from 'react'
-import Swal from 'sweetalert2'
-
 
 function LinkForm(props) {
     const userValues = {
@@ -14,18 +12,12 @@ function LinkForm(props) {
     const handleInputChange = e =>{
         const {name, value} = e.target;
         setValues({...values, [name]: value});
-        
     }
 
     const handleSubmit = e => {
         e.preventDefault();
-        console.log(values);
-        props.addOrEdit();
-        Swal.fire(
-            'The Internet?',
-            'That thing is still around?',
-            'success'
-        )
+        props.addOrEdit(values);
+        setValues({...userValues});
     }
     return (
         <div>
@@ -33,21 +25,21 @@ function LinkForm(props) {
                 <div className="row p-4 g-3">
                     <div className="col-md-6">
                         <label className="form-control rounded shadow-lg"><i className="fas fa-user"></i> Nombre de usuario</label>
-                        <input onChange={handleInputChange} type="text" name="name" id="name" className="form-control rounded shadow-lg" placeholder="Nombre de usuario" />
+                        <input onChange={handleInputChange} value={values.name|| ''} type="text" name="name" id="name" className="form-control rounded shadow-lg" placeholder="Nombre de usuario" />
                     </div>
                     <div className="col-md-6">
                         <label className="form-control rounded shadow-lg"><i className="fas fa-envelope"></i> Correo electronico</label>
-                        <input onChange={handleInputChange} type="email" name="email" id="email" className="form-control rounded shadow-lg" placeholder="Correo electronico" />
+                        <input onChange={handleInputChange} value={values.email|| ''} type="email" name="email" id="email" className="form-control rounded shadow-lg" placeholder="Correo electronico" />
                     </div>
                 </div>
                 <div className="row p-4 g-3">
                     <div className="col-md-6">
                         <label className="form-control rounded shadow-lg"><i className="fas fa-phone"></i> Telefono</label>
-                        <input onChange={handleInputChange} type="number" name="phone" id="phone" className="form-control rounded shadow-lg" placeholder="Telefono" />
+                        <input onChange={handleInputChange} value={values.phone || ''} type="number" name="phone" id="phone" className="form-control rounded shadow-lg" placeholder="Telefono" />
                     </div>
                     <div className="col-md-6">
                         <label className="form-control rounded shadow-lg"><i className="fas fa-map-marker-alt"></i> Direccion</label>
-                        <textarea onChange={handleInputChange} className="form-control rounded shadow-lg" name="address" id="address" cols="5" rows="5"
+                        <textarea onChange={handleInputChange} value={values.address|| ''} className="form-control rounded shadow-lg" name="address" id="address" cols="5" rows="5"
                             placeholder="Direccion"></textarea>
                     </div>
                 </div>
--- src/components/Links.js
+++ src/components/Links.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import LinkForm from './LinkForm'
+import React, { useEffect, useState } from 'react';
+import LinkForm from './LinkForm';
+import { database } from '../firebase';
+import Swal from 'sweetalert2';
 
 function Links() {
 
-    const addUser = () => {
-        console.log('Added user');
+    const [users, setUsers] = useState([]);
+    const addOrEditUser = async (userObject) => {
+        await database.collection('users').doc().set(userObject).then((results) => {
+            Swal.fire('Usuario agregado', 'El usuario se registro correctamente.', 'success');
+        }).catch((errors) => {
+            Swal.fire('Error', 'Ocurrio un error al agregar el usuario', 'error');
+        });
     }
+
+    const getUsers = async () => {
+        database.collection("users").onSnapshot((querySnapshot) => {
+            const docs = [];
+            querySnapshot.forEach((doc) => {
+                docs.push({ ...doc.data(), id: doc.id });
+            });
+            setUsers(docs);
+        });
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
     return (
         <div>
             <div className="container-fluid">
@@ -14,7 +36,37 @@ function Links() {
                 </div>
                 <div className="row">
                     <div className="col-md-12">
-                        <LinkForm addOrEdit={addUser}></LinkForm>
+                        <LinkForm addOrEdit={addOrEditUser}></LinkForm>
+                    </div>
+                </div>
+                <div className="container p-4">
+                    <div className="row p-2 text-center">
+                        <h1>Usuarios disponibles</h1>
+                        <hr />
+                    </div>
+                    <div className="row p-4">
+                        <div className="col-md-12">
+                            <table className="table table-striped shadow-lg">
+                                <thead className="bg-dark text-white">
+                                    <tr>
+                                        <th><i class="far fa-user"></i> Nombre</th>
+                                        <th><i class="far fa-envelope"></i>  Email</th>
+                                        <th><i class="fas fa-phone-square"></i> Telefono</th>
+                                        <th><i class="fas fa-map-marker-alt"></i> Direccion</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {users.map(user => (
+                                        <tr>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                            <td>{user.phone}</td>
+                                            <td>{user.address}</td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
